12506df55c11ebdeafc90459d4d57c3400d18818
GG <gerencser@rocketmail.com>
2020-02-11 23:34:36 +0000

    Changed sql user host to general host

--- demo/database/database.sql
+++ demo/database/database.sql
@@ -7,6 +7,4 @@ CREATE TABLE date (
         comment CHAR(150) NOT NULL DEFAULT '0',
         PRIMARY KEY (`id`)
 );
-GRANT ALL PRIVILEGES ON demodb.* TO 'demo_user'@'10.0.1.101' IDENTIFIED BY 'Password12';
-GRANT ALL PRIVILEGES ON demodb.* TO 'demo_user'@'10.0.1.102' IDENTIFIED BY 'Password12';
-GRANT ALL PRIVILEGES ON demodb.* TO 'demo_user'@'10.0.1.103' IDENTIFIED BY 'Password12';
+GRANT ALL PRIVILEGES ON demodb.* TO 'demo_user'@'%' IDENTIFIED BY 'Password12';
